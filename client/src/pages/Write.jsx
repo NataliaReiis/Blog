64b6cc7436c5203854905a24ce204543194a50b8
NataliaReiis <nataliareis208@gmail.com>
2024-01-07 21:58:49 -0300

    feat: include ReactQuill

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css';
 
 export default function Write(){
+    const [value, setValue] = useState('')
     return(
-        <div className="write">
-            <div className="content">content</div>
+        <div className="write-add">
+            <div className="content">
+                <input type="text" placeholder='Title' />
+                <div className="editorContainer">
+                    <ReactQuill theme='snow' value={value} onChange={setValue} />
+                </div>
+            </div>
             <div className="menu">
                 <div className="item">i1</div>
                 <div className="item">i2</div>
