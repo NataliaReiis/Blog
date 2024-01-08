886ab68b09d68ff7dcc500639439931a4a0c5a1b
NataliaReiis <nataliareis208@gmail.com>
2024-01-07 22:53:24 -0300

    feat: finish write / editor

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,21 +1,65 @@
-import { useState } from 'react';
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
-export default function Write(){
-    const [value, setValue] = useState('')
-    return(
-        <div className="write-add">
-            <div className="content">
-                <input type="text" placeholder='Title' />
-                <div className="editorContainer">
-                    <ReactQuill theme='snow' value={value} onChange={setValue} />
-                </div>
-            </div>
-            <div className="menu">
-                <div className="item">i1</div>
-                <div className="item">i2</div>
-            </div>
+export default function Write() {
+  const [value, setValue] = useState("");
+  return (
+    <div className="write-add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
-    )
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Pubish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="science" value="science" id="science" />
+            <label htmlFor="art">Science</label>
+          </div>
+          <div className="cat">
+            <input
+              type="radio"
+              name="technology"
+              value="technology"
+              id="technology"
+            />
+            <label htmlFor="art">Technology</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="design" value="design" id="design" />
+            <label htmlFor="art">Design</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
