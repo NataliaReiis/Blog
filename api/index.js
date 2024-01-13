1e3b7596ec8ac7c8cbd9b08bbc23db11269e5a8a
NataliaReiis <nataliareis208@gmail.com>
2024-01-12 23:38:28 -0300

    feat: routes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 import express from "express"
+import postRoutes from "./routes/posts.js"
 
 const app = express()
 
 app.use(express.json())
+
+
+
+app.use("/api/posts", postRoutes)
 
 app.listen(8080, ()=>{
     console.log("Conexão realizada com sucesso!")
